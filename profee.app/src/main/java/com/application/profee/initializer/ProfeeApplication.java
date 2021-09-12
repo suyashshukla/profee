ce4e8b97ef884bfe66ef7d2ca4bc24be243e5fbf
Suyash Shukla <suyash.s@keka.com>
2021-09-12 11:02:16 +0530

    Added proxy conf to the client application

--- profee.app/src/main/java/com/application/profee/initializer/ProfeeApplication.java
+++ profee.app/src/main/java/com/application/profee/initializer/ProfeeApplication.java
@@ -1,13 +1,20 @@
 package com.application.profee.initializer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.autoconfigure.ConfigurationPropertiesRebinderAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.application.profee.services", "com.application.profee.initializer", "com.application.profee.controllers"})
 @EntityScan(basePackages = "com.application.profee.data")
 @EnableJpaRepositories("com.application.profee.repositories")
+@EnableZuulProxy
 public class ProfeeApplication {
 
   public static void main(String[] args) {
